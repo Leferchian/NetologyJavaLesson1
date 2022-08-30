129b71ebd0aeb13588901bcd2c2c9fa89115334a
Egor Boyko <leferchian@gmail.com>
2022-08-30 20:44:08 +0300

    paste code from homework

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         int eaters = 5; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
-        int potatoes = 4; // картофелин
+        int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
         int spices = 10; // ложек специй
 
